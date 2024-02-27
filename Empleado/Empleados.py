b793fe5862a3cb10ab77073ac980f67cce86a206
Andres Camilo Rios <acamilorm4556@gmail.com>
2024-02-27 00:14:39 -0500

    Métodos del empleado creados

--- Empleado/Empleados.py
+++ Empleado/Empleados.py
@@ -8,3 +8,30 @@ class Empleado:
     # 1 = Masculino 2 = Femenino ---"""
     sexo=0 
     salario=0
+
+    """------------------------------
+    # Metodos
+    --------------------------------"""
+    def CambiarSalario(self, nuevoSalario):
+        #Aquí va el código del método
+        return 0
+
+    def CambiarEmpleado(self, nNombre, nApellido, nSexo, nSalario):
+        #Aquí va el código del método
+        return None
+
+    def ConsultarSalario(self):
+        #Aquí va el código dek método
+        return self.salario
+
+    def NombreCompleto(self):
+        #Aquí va el código del método
+        return self.nombre
+    
+    def ApellidoCompleto(self):
+        #Aquí va el código del método
+        return self.apellido
+    
+    def consultarSexo(self):
+        #Aquí va el código del método
+        return self.sexo
