0d1a93265d5c743b7000ec8bb725451f62f103bb
AndresCamilo30 <acamilorm4556@gmail.com>
2024-02-27 00:52:14 -0500

    Creados los métodos de Simulador bancario

--- SimuladorBancario/SimuladorBancario.py
+++ SimuladorBancario/SimuladorBancario.py
@@ -6,3 +6,19 @@ class SimuladorBancario:
     cedula = ""
     nombre = ""
     mesActual = 0
+
+    """---------------------------------------------------------
+    # METODOS
+    ---------------------------------------------------------"""
+
+    def ConsignarValor(self):
+        #Aquí va el código del método
+        return 0
+    
+    def RetirarValor(self):
+        #Aquí va el código del método
+        return 0
+    
+    def DarInteresMensual(self):
+        #Aquí va el código del método
+        return 0
